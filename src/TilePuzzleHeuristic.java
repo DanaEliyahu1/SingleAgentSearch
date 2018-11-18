b36ed5fd792b36f3e62124f2f65b82e268edad55
dana <danotbhd@gmail.com>
2018-11-18 22:15:20 +0200

    DONE HIORISTIC

--- src/TilePuzzleHeuristic.java
+++ src/TilePuzzleHeuristic.java
@@ -5,6 +5,15 @@ public class TilePuzzleHeuristic implements IHeuristic
 	@Override
 	public double getHeuristic(IProblemState problemState)
 	{
+		if(problemState.isGoalState()){
+			return 0;
+		}
+		else {
+
+		}
+
+
+
 		return 0;
 	}
 	
