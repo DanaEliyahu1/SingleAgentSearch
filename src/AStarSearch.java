e6ef42171f2c117068865617cf6bfa087a4611da
dana <danotbhd@gmail.com>
2018-11-18 11:57:18 +0200

    DONE HIORISTIC

--- src/AStarSearch.java
+++ src/AStarSearch.java
@@ -1,7 +1,12 @@
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 
 public class AStarSearch   extends ASearch
 {
 	// Define lists here ...
+	PriorityQueue<ASearchNode> openlist;
+	HashSet<ASearchNode> closelist;
 	
 	@Override
 	public String getSolverName() 
@@ -10,8 +15,7 @@ public class AStarSearch   extends ASearch
 	}
 	
 	@Override
-	public ASearchNode createSearchRoot
-	(
+	public ASearchNode createSearchRoot(
 		IProblemState problemState
 	) 
 	{	
@@ -20,66 +24,69 @@ public class AStarSearch   extends ASearch
 	}
 
 	@Override
-	public void initLists() 
+	public void initLists()
 	{
-		
+		openlist = new PriorityQueue<>(new Comparator<ASearchNode>() {
+			@Override
+			public int compare(ASearchNode o1, ASearchNode o2) {
+				return (int)(o1.getF()-o2.getF());
+			}
+		});
+		closelist = new HashSet<>();
 	}
 
 	@Override
-	public ASearchNode getOpen
-	(
-		ASearchNode node
-	) 
+	public ASearchNode getOpen(ASearchNode node)
 	{
+		if (isOpen(node)){
+			return node;
+		}
 		return null;
 	}
 
 	@Override
-	public boolean isOpen
-	(
-		ASearchNode node
-	) 
+	public boolean isOpen(ASearchNode node)
 	{
-		return false;
-	}
-	
-	@Override
-	public boolean isClosed
-	(
-		ASearchNode node
-	) 
-	{
+		if (openlist.contains(node)) {
+			return true;
+		}
 		return false;
 	}
 
-	@Override
-	public void addToOpen
-	(
-		ASearchNode node
-	) 
-	{
-
-	}
 
 	@Override
-	public void addToClosed
-	(
-		ASearchNode node
-	) 
+	public boolean isClosed(ASearchNode node)
 	{
-		
+		if (closelist.contains(node)) {
+			return true;
+		}
+		return false;
 	}
+
+
+
+	@Override
+	public void addToOpen(ASearchNode node){
+		openlist.add(node);
+	}
+
+
+	@Override
+	public void addToClosed(ASearchNode node){
+		closelist.add(node);
+	}
+
 
 	@Override
 	public int openSize() 
 	{
-		return 0;
+		return openlist.size();
 	}
 
 	@Override
 	public ASearchNode getBest() 
 	{
-		return null;
+		return openlist.poll();
 	}
 
 }
